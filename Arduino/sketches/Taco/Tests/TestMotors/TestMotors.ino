d13bfbf0dbeeff834f594522530669154ea3577b
Scottpaul1 <scott.paul@frsu38.org>
2017-03-16 07:54:10 -0400

    TestMotor Update
    Code used to move robot in a square for trouleshooting purposes.

--- Arduino/sketches/Taco/Tests/TestMotors/TestMotors.ino
+++ Arduino/sketches/Taco/Tests/TestMotors/TestMotors.ino
@@ -1,3 +1,4 @@
+#define DEBUG
 #include <Taco.h>
 #include <Arduino.h>
 #include <Adafruit_MotorShield.h>
@@ -8,9 +9,30 @@ RobotController* RC;
 void setup() {
   RC = new RobotController();
   RC->initialize();
-  RC->go(North, DEFAULT_SPEED, NoSide, 0, NoRotation, 0);
   delay(5000);
+  Serial.begin(9600);
+  /*
+  RC->go(North, DEFAULT_SPEED, NoSide, 0, NoRotation, 0);
+  delay(2000);
   RC->stop();
+  RC->go(North, 0, Right, DEFAULT_SPEED, NoRotation, 0);
+  delay(2000);
+  RC->stop();
+  */
+  RC->go(South, DEFAULT_SPEED, NoSide, 0, NoRotation, 0);
+  delay(2000);
+  RC->stop();
+  /*
+  RC->go(North, 0, Left, DEFAULT_SPEED, NoRotation, 0);
+  delay(2000);
+  RC->stop();
+  RC->go(North, 0, NoSide, 0, CW, DEFAULT_SPEED);
+  delay(2000);
+  RC->stop();
+  RC->go(North, 0, NoSide, 0, CCW, DEFAULT_SPEED);
+  delay(2000);
+  RC->stop();
+  */
 }
 
 void loop() {
